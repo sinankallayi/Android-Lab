b7cc810591e23f03add39dec6bf13997f819a0ff
sinankallayi <71578512+sinankallayi@users.noreply.github.com>
2024-08-07 13:05:38 +0530

    prgrm 2

---
+++ Activity Lifecycle/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
